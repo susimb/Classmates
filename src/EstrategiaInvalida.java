277e95940a02d7b55f89f282ed6b4a36ef87e95c
susimb <susimarinbotero@gmail.com>
2026-05-21 05:55:34 -0500

    changes

--- src/EstrategiaInvalida.java
+++
@@ -1,7 +0,0 @@
-public class EstrategiaInvalida extends Errores{
-    public EstrategiaInvalida() {
-            super(
-                    "La estrategia es inválida."
-            );
-        }
-    }
--- src/ModeracionInsuficiente.java
+++
@@ -1,7 +0,0 @@
-public class ModeracionInsuficiente extends Errores{
-    public ModeracionInsuficiente() {
-        super(
-                "No tiene permisos de moderación."
-            );
-        }
-    }
